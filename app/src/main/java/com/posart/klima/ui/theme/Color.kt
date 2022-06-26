83afc5929c855b1269eae7e6146d73081b17ba4f
Artur Bruno <arturjuliao20@gmail.com>
2022-06-26 16:37:14 -0300

    feat(style): Apply app style
    Place application colors and typography on the MaterialTheme object to
    apply default styles.

--- app/src/main/java/com/posart/klima/ui/theme/Color.kt
+++ app/src/main/java/com/posart/klima/ui/theme/Color.kt
@@ -2,10 +2,72 @@ package com.posart.klima.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light colors
+val md_theme_light_primary = Color(0xFF005CBC)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFD7E2FF)
+val md_theme_light_onPrimaryContainer = Color(0xFF001B3F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_light_secondary = Color(0xFF006E0C)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFF92FA83)
+val md_theme_light_onSecondaryContainer = Color(0xFF002201)
+
+val md_theme_light_tertiary = Color(0xFF795900)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFDF9F)
+val md_theme_light_onTertiaryContainer = Color(0xFF261A00)
+
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+val md_theme_light_background = Color(0xFFF6FEFF)
+val md_theme_light_onBackground = Color(0xFF001F24)
+
+val md_theme_light_surface = Color(0xFFF6FEFF)
+val md_theme_light_onSurface = Color(0xFF001F24)
+val md_theme_light_surfaceVariant = Color(0xFFE0E2EC)
+val md_theme_light_onSurfaceVariant = Color(0xFF44474E)
+
+val md_theme_light_outline = Color(0xFF74777F)
+
+val md_theme_light_inverseOnSurface = Color(0xFFD0F8FF)
+val md_theme_light_inverseSurface = Color(0xFF00363D)
+val md_theme_light_inversePrimary = Color(0xFFABC7FF)
+
+// Light colors
+val md_theme_dark_primary = Color(0xFFABC7FF)
+val md_theme_dark_onPrimary = Color(0xFF002F66)
+val md_theme_dark_primaryContainer = Color(0xFF004590)
+val md_theme_dark_onPrimaryContainer = Color(0xFFD7E2FF)
+
+val md_theme_dark_secondary = Color(0xFF77DD6A)
+val md_theme_dark_onSecondary = Color(0xFF003A03)
+val md_theme_dark_secondaryContainer = Color(0xFF005307)
+val md_theme_dark_onSecondaryContainer = Color(0xFF92FA83)
+
+val md_theme_dark_tertiary = Color(0xFFF9BD1B)
+val md_theme_dark_onTertiary = Color(0xFF402D00)
+val md_theme_dark_tertiaryContainer = Color(0xFF5B4300)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFDF9F)
+
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val md_theme_dark_background = Color(0xFF001F24)
+val md_theme_dark_onBackground = Color(0xFF97F0FF)
+
+val md_theme_dark_surface = Color(0xFF001F24)
+val md_theme_dark_onSurface = Color(0xFF97F0FF)
+val md_theme_dark_surfaceVariant = Color(0xFF44474E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC4C6D0)
+
+val md_theme_dark_outline = Color(0xFF8E9099)
+
+val md_theme_dark_inverseOnSurface = Color(0xFF001F24)
+val md_theme_dark_inverseSurface = Color(0xFF97F0FF)
+val md_theme_dark_inversePrimary = Color(0xFF005CBC)
--- app/src/main/java/com/posart/klima/ui/theme/Type.kt
+++ app/src/main/java/com/posart/klima/ui/theme/Type.kt
@@ -2,33 +2,125 @@ package com.posart.klima.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.posart.klima.R
 
-// Set of Material typography styles to start with
+val Lato = FontFamily(
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_light, FontWeight.Light),
+    Font(R.font.lato_black, FontWeight.Black)
+)
+
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    labelLarge = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Lato,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        letterSpacing = 0.10000000149011612.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 11.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 40.sp,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 36.sp,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 32.sp,
+        fontSize = 24.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 64.sp,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 52.sp,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 44.sp,
+        fontSize = 36.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 28.sp,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
 )
